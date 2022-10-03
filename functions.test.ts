09fabb42e750d3d3bc656926d91c0019624d5e36
Woody White <woody.white276@gmail.com>
2022-10-03 12:07:20 -0400

    initial committ

--- functions.test.ts
+++ functions.test.ts
@@ -1,8 +1,18 @@
 const { shuffleArray } = require("./utils");
 
 describe("shuffleArray should", () => {
-  test("shuffleArray should return the same length", () => {
-    let shuffledArray = shuffleArray(shuffleArray.arrCopy);
-    expect(shuffleArray.length).toEqual(shuffleArray.arrCopy.length);
+  test("shuffleArray should return the same length after the function", () => {
+    let arr = [1, 2, 3, 4, 5];
+    let result = shuffleArray(arr);
+    expect(arr.length).toBe(result.length);
+  });
+  test("shuffleArray should return array with same items", () => {
+    //create array
+    //run through shuffle array function
+    //check to make sure all original values are included in new array
+    let arr = [1, 2, 3, 4, 5];
+    let result = shuffleArray(arr);
+
+    expect(result).toEqual(expect.arrayContaining(arr));
   });
 });
